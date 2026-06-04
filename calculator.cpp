d58003b32711049f549a9ee7da6dea4d069a4d90
Nitin singh <nitinsingh27072008@gmail.com>
2026-06-04 23:15:22 +0530

    Create calculator.cpp

---
+++ calculator.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float a, b;
+    char op;
+
+    cin >> a >> op >> b;
+
+    switch(op) {
+        case '+': cout << a + b; break;
+        case '-': cout << a - b; break;
+        case '*': cout << a * b; break;
+        case '/': cout << a / b; break;
+        default: cout << "Invalid Operator";
+    }
+
+    return 0;
+}
